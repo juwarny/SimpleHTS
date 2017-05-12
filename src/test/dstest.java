4a8837340b7363e10bd2dc1fabb50bb4dbe3da53
zmsdp <zmsdp@DESKTOP-7S0OBDJ>
2017-05-12 18:23:51 +0900

    screentest

--- src/test/dstest.java
+++ src/test/dstest.java
@@ -1,7 +1,11 @@
 package test;
 
+import com4j.*;
+import test.cputil.*;
+
 public class dstest {
 	public static void main(String[] args){
-		
+		ICpCybos bos = ClassFactory.createCpCybos();
+		System.out.println(bos.isConnect());
 	}
 }
---
+++ src/test/mainScreen.java
@@ -0,0 +1,76 @@
+package test;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
+import javax.swing.JDesktopPane;
+import javax.swing.JTable;
+import javax.swing.JList;
+import javax.swing.JTree;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollBar;
+import java.awt.Font;
+
+public class mainScreen extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainScreen frame = new mainScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public mainScreen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1078, 644);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JToolBar toolBar = new JToolBar();
+		contentPane.add(toolBar, BorderLayout.NORTH);
+		
+		JButton btnNewButton = new JButton("계정");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		toolBar.add(btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("전략");
+		toolBar.add(btnNewButton_2);
+		
+		JButton btnNewButton_1 = new JButton("설정");
+		toolBar.add(btnNewButton_1);
+		
+		table = new JTable();
+		table.setFont(new Font("나눔고딕", Font.PLAIN, 12));
+		contentPane.add(table, BorderLayout.CENTER);
+		
+		JList list = new JList();
+		contentPane.add(list, BorderLayout.EAST);
+	}
+
+}
